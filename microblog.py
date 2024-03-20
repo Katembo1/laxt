428dd12f813b01eb7fa1d91d5047a70c9433d942
japhet <23198@student.embuni.ac.ke>
2024-03-20 19:28:14 +0300

    0.22

--- microblog.py
+++ microblog.py
@@ -1,12 +1,9 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as so
-from app import create_app, db
-from app.models import User, Post
-
+from app import create_app, db, cli
+from app.models import User, Post, Message, Notification, Task
 app = create_app()
-
-
+cli.register(app)
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post, 'Message': Message,
+         'Notification': Notification, 'Task': Task}
 app.run(debug=True)
